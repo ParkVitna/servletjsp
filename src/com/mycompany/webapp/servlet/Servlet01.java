182a375ce5aef96b18bb10d97326ebea6496cf8d
ParkVitna <bbn960903@gmail.com>
2021-03-15 12:54:45 +0900

    3월 15일 오전수업 servletjsp

---
+++ src/com/mycompany/webapp/servlet/Servlet01.java
@@ -0,0 +1,28 @@
+package com.mycompany.webapp.servlet;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Servlet01")
+public class Servlet01 extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("Servlet01 - service() 실행");
+		
+		//요청 내용을 확인
+		System.out.println("클라이언트 IP:" + req.getRemoteHost());
+		//요청 처리 -> 지금을 할 수 없음
+		
+		//응답 내용을 생성 및 전송
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/views/view01.jsp");
+		rd.forward(req, resp); //이쪽으로 이동해
+		
+	}
+}
